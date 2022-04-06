b37f49f75747eec480450ebfd3c2c6767896c78c
JHao <985206133@qq.com>
2022-04-06 18:19:18 +0800

    0.2.0
    reconsitution

--- myNote/notesHome.js
+++ myNote/notesHome.js
@@ -1,21 +1,21 @@
+//导航栏事件
 {
-    //导航栏目录的展开关闭
+    //目录开合
     (function myNavClickEvent() {
         let myNav = document.getElementsByTagName('myNav');
         let lastOpenNavDom = null
         for (let el of myNav) {
             if (el.children.length > 1) {
                 el.addEventListener('click', () => {
-                    el.children[1].className = el.children[1].className === 'innerUlShow' ? 'innerUl' : 'innerUlShow'
+                    el.children[1].style.display = el.children[1].style.display === 'block' ? 'none' : 'block'
                     changeArrowDir(el.children[0])
                     if (lastOpenNavDom &&
                         lastOpenNavDom !== el &&
-                        lastOpenNavDom.children[1].className === 'innerUlShow') {
+                        lastOpenNavDom.children[1].style.display === 'block') {
                         changeArrowDir(lastOpenNavDom.children[0])
-                        lastOpenNavDom.children[1].className = 'innerUl'
+                        lastOpenNavDom.children[1].style.display = 'none'
                     }
                     lastOpenNavDom = el
-                    // el.children[1].className = el.children[1].className === 'innerUlShow' ? 'innerUl' : 'innerUlShow'
                 });
                 el.children[1].addEventListener('click', function (e) {
                     e.stopPropagation()
