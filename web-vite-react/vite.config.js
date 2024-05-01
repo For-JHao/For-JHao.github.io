4ce98a3a3b321c90dcc5ef60ff00edb4aded96f8
JHao <985206133@qq.com>
2024-05-02 00:16:11 +0800

    add notes request

--- web-vite-react/vite.config.js
+++ web-vite-react/vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // define: {
-  //   'import.meta.env': JSON.stringify(process.env),
-  // },
+  publicDir:'../myNote/note/learningNotes',
+  assetsInclude:['*.md']
 })
